ce97b8cf43f1f3ab02d75f3f95acf029590661ab
kang_deok <77061558+rkdejr2321@users.noreply.github.com>
2023-09-25 23:16:10 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        String num = Integer.toString(x);
+        int sum = 0;
+        
+        for (int i = 0; i < num.length(); i++) {
+            sum += num.charAt(i) - '0';
+        }
+        
+        if (x % sum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
